af81fad4b9a08a43f87a03bcb4685bc0352976a4
Andrii <andmos@st.amu.edu.pl>
2019-03-21 09:31:41 +0100

    minor changes to 2.2

--- lesson2.2.py
+++ lesson2.2.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
+def autolabel(rects):
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                str(height),
+                ha='center', va='bottom')
+
 filename = "files/bars.txt"
 file = open(filename, "r")
 tmp = []
@@ -10,5 +18,7 @@ x = [float(i) for i in tmp[0].split()]
 y = [float(i) for i in tmp[1].split()]
 
 for cx, cy in zip(x, y):
-    plt.bar(cx, cy, 0.5, color=[cy, 0, cy, 1])
+    rect = plt.bar(cx, cy, 0.5, color=[cy, 0, cy, 1])
+    autolabel(rect)
+
 plt.show()
